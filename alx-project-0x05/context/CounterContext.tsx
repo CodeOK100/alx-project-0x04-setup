e8b84937420162757726383b99459507c905a06e
Enoch Okine Mensah <okaycode100@gmail.com>
2025-10-01 12:45:42 +0000

    Included a ContextAPI for global state management

---
+++ alx-project-0x05/context/CounterContext.tsx
@@ -0,0 +1,25 @@
+import { CounterContextProps } from "@/interface";
+import { createContext, useContext, useState, ReactNode } from "react";
+
+const CounterContext = createContext<CounterContextProps | undefined>(undefined);
+
+export const CounterProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => setCount((count) => count + 1);
+  const decrement = () => setCount((count) => (count > 0 ? count - 1 : 0));
+
+  return (
+    <CounterContext.Provider value={{ count, increment, decrement }}>
+      {children}
+    </CounterContext.Provider>
+  );
+}
+
+export const useCounter = (): CounterContextProps => {
+  const context = useContext(CounterContext);
+  if (!context) {
+    throw new Error("useCounter must be used within a CounterProvider");
+  }
+  return context;
+};
---
+++ alx-project-0x05/pages/_app.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout/Layout";
+import { CounterProvider } from "@/context/CounterContext";
+import "@/styles/globals.css";
+import type {AppProps} from "next/app";
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    < CounterProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CounterProvider>
+  );
+}
